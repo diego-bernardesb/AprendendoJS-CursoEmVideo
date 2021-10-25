38bf898fd5eff4717ea6143b95a2f6dc5ab93ba9
diego-bernardesb <diegobbabo@gmail.com>
2021-10-25 15:44:50 -0300

    Estruturas de repetição While e Do While

---
+++ Aula 13/Ambiente.js
@@ -0,0 +1,39 @@
+   	var temFatia = 1
+    while(temFatia <= 8){  
+        console.log(`Comi a fatia ${temFatia}`)
+        temFatia++
+        }	
+     
+
+
+
+
+/*var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6)
+*/
+
+
+
+
+/*var c = 1
+while (c <= 6){
+    console.log(`Conte ${c}`)
+    c++ //c = c + 1
+}*/
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
